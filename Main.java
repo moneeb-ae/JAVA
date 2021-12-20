6bfe650c9dee8d7410dc91ff41e2329e3322a2a9
moneeb-ae <57269603+moneeb-ae@users.noreply.github.com>
2021-12-20 00:42:03 -0600

    First Java Program
    Goals: Print "hello world" and to get use to Eclipse IDE.

---
+++ Main.java
@@ -0,0 +1,12 @@
+package JV1_Print_HelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("First Java Program!");
+		//NOTE: System is with a capitol S.
+	}
+
+}
