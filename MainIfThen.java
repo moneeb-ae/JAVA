1abbbba24f7497ac20fd71be15e9ca2abfe764c7
moneeb-ae <57269603+moneeb-ae@users.noreply.github.com>
2021-12-23 02:44:09 -0600

    Practicing different types of loops.

---
+++ MainIfThen.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class MainIfThen {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 16;
+		if ((a>b)&&(a>c))
+			{System.out.println("a is the biggest variable.");}
+		else if ((b>a)&&(b>c))
+		{System.out.println("b is the biggest Variable.");}
+		else {System.out.println("c is the biggest Variable.");}
+	}
+}
---
+++ MainWhile.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class MainWhile {
+
+	public static void main(String[] args) {
+		int greeting = 0;
+		while(greeting < 10){
+			System.out.println("Hello!");
+			greeting++;
+		}
+		
+
+	}
+
+}
